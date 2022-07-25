bf08b3123f0a31cb433f6775b942b197f1ec0569
Maximiliano <maximilianoabelmarcos@gmail.com>
2022-07-24 21:57:20 -0300

    trabajo complementario

---
+++ js/probando.js
@@ -0,0 +1,7 @@
+let pregunta = prompt("Describa sus puntos fuertes");
+
+while (isNaN(pregunta)) {
+    pregunta = alert("Mensaje guardado!")
+
+    pregunta = prompt("agregue otra cualidad o dejenos su numero y lo contactaremos");
+}
