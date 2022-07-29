15059628bf4167cbe7d5b3a044f88474ca19d0ab
Maximiliano <maximilianoabelmarcos@gmail.com>
2022-07-29 19:46:46 -0300

    1er trabajo entregable. PRODE

--- js/probando.js
+++ js/probando.js
@@ -1,7 +1,65 @@
-let pregunta = prompt("Describa sus puntos fuertes");
 
-while (isNaN(pregunta)) {
-    pregunta = alert("Mensaje guardado!")
+/*
 
-    pregunta = prompt("agregue otra cualidad o dejenos su numero y lo contactaremos");
+for (let partidosMundial = 1; partidosMundial <= 3; partidosMundial++) {
+
+    let pregunta = +prompt("Pronosticar cuantos pts sumará Arg (G=3,E=1,P=0)");
+    let almacenar = pregunta;
+    almacenar = almacenar + pregunta;
+
+    if (isNaN(pregunta)) {
+        break;
+    } else {
+        console.log("Partido N° " + partidosMundial + " Argentina suma " + almacenar + " puntos"); }
+} */
+
+
+
+
+let puntos = 0;
+let ganados = 0;
+let empatados = 0;
+let perdidos = 0;
+let suma = 0;
+
+
+for (let partidosMundial = 1; partidosMundial <= 3; partidosMundial++) {
+
+    let pregunta = prompt("Pronosticar cuantos pts sumará Arg (G=3,E=1,P=0)");
+
+   /*if (isNaN(pregunta)) {
+        break;
+    } else {
+        console.log("Partido N° " + partidosMundial + " Argentina suma " + pregunta + " puntos"); }*/
+
+switch (pregunta){
+case "G":
+suma = 3;
+puntos = puntos + suma;
+ganados++;
+break;
+
+case "E":
+suma = 1;
+puntos = puntos + suma;
+empatados++;
+break;
+
+case "P":
+suma = 0;
+perdidos++;
+break;
+
+default:
+alert("Valor ingresado no válido\nEl partido será invalidado, vuelva a cargar la pagina");
+break;
 }
+console.log("Partido N° " + partidosMundial + " Argentina suma " + suma + " puntos");
+console.log("En " + partidosMundial + " fechas " + " Argentina suma " + puntos + " puntos");
+console.log("Resultados:\nPartidos ganados: "+ganados+"\nPartidos empatados: "+empatados+"\nPartidos perdidos: "+perdidos);
+
+}
+
+
+
+
